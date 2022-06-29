54daac571dc07389fe66dc4ec86cc764a7a486c6
Matthijs Kamstra <matthijs@fonk.amsterdam>
2022-06-29 13:17:35 +0200

    dag horizontal

--- README.md
+++ README.md
@@ -10,6 +10,12 @@ This is a [Haxe](http://www.haxe.org) project, read more about it in the [README
 
 - https://dev.to/dailydevtips1/making-divs-user-resizable-with-css-3ga
 - https://medium.com/the-z/making-a-resizable-div-in-js-is-not-easy-as-you-think-bda19a1bc53d
+- https://htmldom.dev/make-a-resizable-element/
+
+#### drag
+
+- https://www.w3schools.com/howto/howto_js_draggable.asp
+- https://www.kirupa.com/html5/drag.htm
 
 Making a resizable div in JS is not easy as you think | by Nguyễn Việt Hưng | The happy lone guy | Medium
 
--- docs/example_js_gitlab.js
+++ docs/example_js_gitlab.js
@@ -1,117 +1,66 @@
 (function ($global) { "use strict";
 var Main = function() {
-	console.log("src/Main.hx:15:","Hello 'Example JS Gitlab'");
-	this.makeResizableDiv(".resizable");
+	console.log("src/Main.hx:11:","Main");
+	this.initDrag(window.document.getElementById("resizeMe"));
 };
 Main.main = function() {
 	var app = new Main();
 };
 Main.prototype = {
-	makeResizableDiv: function(div) {
-		var element = window.document.querySelector(div);
-		var resizers = window.document.querySelectorAll(div + " .resizer");
-		var minimum_size = 20;
-		var original_width = 0;
-		var original_height = 0;
-		var original_x = 0;
-		var original_y = 0;
-		var original_mouse_x = 0;
-		var original_mouse_y = 0;
-		var _g = 0;
-		var _g1 = resizers.length;
-		while(_g < _g1) {
-			var i = _g++;
-			var currentResizer = [resizers[i]];
-			var resize = [(function(currentResizer) {
-				return function(e) {
-					if(currentResizer[0].classList.contains("bottom-right")) {
-						var width = original_width + (e.pageX - original_mouse_x);
-						var height = original_height + (e.pageY - original_mouse_y);
-						if(width > minimum_size) {
-							element.style.width = width + "px";
-						}
-						if(height > minimum_size) {
-							element.style.height = height + "px";
-						}
-					} else if(currentResizer[0].classList.contains("bottom-left")) {
-						var height = original_height + (e.pageY - original_mouse_y);
-						var width = original_width - (e.pageX - original_mouse_x);
-						if(height > minimum_size) {
-							element.style.height = height + "px";
-						}
-						if(width > minimum_size) {
-							element.style.width = width + "px";
-							element.style.left = original_x + (e.pageX - original_mouse_x) + "px";
-						}
-					} else if(currentResizer[0].classList.contains("top-right")) {
-						var width = original_width + (e.pageX - original_mouse_x);
-						var height = original_height - (e.pageY - original_mouse_y);
-						if(width > minimum_size) {
-							element.style.width = width + "px";
-						}
-						if(height > minimum_size) {
-							element.style.height = height + "px";
-							element.style.top = original_y + (e.pageY - original_mouse_y) + "px";
-						}
-					} else {
-						var width = original_width - (e.pageX - original_mouse_x);
-						var height = original_height - (e.pageY - original_mouse_y);
-						if(width > minimum_size) {
-							element.style.width = width + "px";
-							element.style.left = original_x + (e.pageX - original_mouse_x) + "px";
-						}
-						if(height > minimum_size) {
-							element.style.height = height + "px";
-							element.style.top = original_y + (e.pageY - original_mouse_y) + "px";
-						}
-					}
-				};
-			})(currentResizer)];
-			var stopResize = [(function(resize) {
-				return function() {
-					window.removeEventListener("mousemove",resize[0]);
-				};
-			})(resize)];
-			currentResizer[0].addEventListener("mousedown",(function(stopResize,resize) {
-				return function(e) {
-					e.preventDefault();
-					original_width = Std.parseInt(StringTools.replace(window.getComputedStyle(element,null).getPropertyValue("width"),"px",""));
-					original_height = Std.parseInt(StringTools.replace(window.getComputedStyle(element,null).getPropertyValue("height"),"px",""));
-					original_x = element.getBoundingClientRect().left | 0;
-					original_y = element.getBoundingClientRect().top | 0;
-					original_mouse_x = e.pageX;
-					original_mouse_y = e.pageY;
-					window.addEventListener("mousemove",resize[0]);
-					window.addEventListener("mouseup",stopResize[0]);
-				};
-			})(stopResize,resize));
-		}
-	}
-};
-var Std = function() { };
-Std.parseInt = function(x) {
-	if(x != null) {
-		var _g = 0;
-		var _g1 = x.length;
-		while(_g < _g1) {
-			var i = _g++;
-			var c = x.charCodeAt(i);
-			if(c <= 8 || c >= 14 && c != 32 && c != 45) {
-				var nc = x.charCodeAt(i + 1);
-				var v = parseInt(x,nc == 120 || nc == 88 ? 16 : 10);
-				if(isNaN(v)) {
-					return null;
-				} else {
-					return v;
-				}
+	initDrag: function(el) {
+		var active = false;
+		var currentX = 0;
+		var currentY = 0;
+		var initialX = 0;
+		var initialY = 0;
+		var xOffset = 0;
+		var yOffset = 0;
+		var dragStart = function(e) {
+			console.log("src/Main.hx:27:","dragstart");
+			console.log("src/Main.hx:28:",e);
+			if(e.type == "touchstart") {
+				initialX = e.touches[0].clientX - xOffset;
+				initialY = e.touches[0].clientY - yOffset;
+			} else {
+				initialX = e.clientX - xOffset;
+				initialY = e.clientY - yOffset;
 			}
-		}
+			if(e.target == el) {
+				active = true;
+				console.log("src/Main.hx:40:",active);
+			}
+		};
+		var dragEnd = function(e) {
+			console.log("src/Main.hx:45:","dragend");
+			initialX = currentX;
+			initialY = currentY;
+			active = false;
+		};
+		var setTranslate = function(xPos,yPos,el) {
+			el.style.transform = "translate3d(" + (xPos == null ? "null" : "" + xPos) + "px, 0px, 0)";
+		};
+		var drag = function(e) {
+			if(active) {
+				e.preventDefault();
+				if(e.type == "touchmove") {
+					currentX = e.touches[0].clientX - initialX;
+					currentY = e.touches[0].clientY - initialY;
+				} else {
+					currentX = e.clientX - initialX;
+					currentY = e.clientY - initialY;
+				}
+				xOffset = currentX;
+				yOffset = currentY;
+				setTranslate(currentX,currentY,e.target);
+			}
+		};
+		el.addEventListener("touchstart",dragStart,false);
+		el.addEventListener("touchend",dragEnd,false);
+		el.addEventListener("touchmove",drag,false);
+		el.addEventListener("mousedown",dragStart,false);
+		el.addEventListener("mouseup",dragEnd,false);
+		el.addEventListener("mousemove",drag,false);
 	}
-	return null;
-};
-var StringTools = function() { };
-StringTools.replace = function(s,sub,by) {
-	return s.split(sub).join(by);
 };
 var haxe_iterators_ArrayIterator = function(array) {
 	this.current = 0;
