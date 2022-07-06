c94a9475ac03ea58f7d87fdeef47db8a0857735c
Matthijs Kamstra <matthijs@fonk.amsterdam>
2022-07-06 11:43:05 +0200

    cleaner table

--- docs/kluez.js
+++ docs/kluez.js
@@ -1,9 +1,36 @@
 (function ($global) { "use strict";
 var MainKluez = function() {
-	console.log("src/MainKluez.hx:5:","MainKluez");
+	console.log("src/MainKluez.hx:24:","MainKluez");
+	this.createTable(100,1000);
 };
 MainKluez.main = function() {
 	var app = new MainKluez();
+};
+MainKluez.prototype = {
+	createTable: function(xTotal,yTotal) {
+		var table0 = window.document.getElementById("container-gantt-kluez").getElementsByClassName("pattern")[0];
+		var colGroup = "";
+		var row = "";
+		var _g = 0;
+		var _g1 = yTotal;
+		while(_g < _g1) {
+			var y = _g++;
+			colGroup = "<colgroup>";
+			row += "<tr>";
+			var _g2 = 0;
+			var _g3 = xTotal;
+			while(_g2 < _g3) {
+				var x = _g2++;
+				colGroup += "<col>";
+				row += "<td><span class=\"table-cell\"></span></td>";
+			}
+			colGroup += "<colgroup>";
+			row += "</tr>";
+		}
+		var t = "<table class=\"table-kluez\">\n" + colGroup + "\n" + row + "</table>";
+		var frag = window.document.createRange().createContextualFragment(t);
+		table0.appendChild(frag);
+	}
 };
 var haxe_iterators_ArrayIterator = function(array) {
 	this.current = 0;
--- src/MainKluez.hx
+++ src/MainKluez.hx
@@ -1,8 +1,52 @@
 package;
 
+import kluez.KlzGrid;
+import kluez.CreateTable;
+import utils.Convert;
+import kluez.CombiElement;
+import const.ClassNames;
+import utils.MathUtil;
+import kluez.El;
+import const.Colors;
+import js.html.Element;
+import kluez.ResizeElement;
+import kluez.DragElement;
+import kluez.CreateElement;
+import kluez.DynamicStyle;
+import js.html.DivElement;
+import js.Browser.*;
+
+using hxColorToolkit.ColorToolkit;
+using StringTools;
+
 class MainKluez {
 	public function new() {
 		trace('MainKluez');
+
+		createTable(100, 1000);
+	}
+
+	public function createTable(xTotal, yTotal) {
+		var table0 = cast document.getElementById('container-gantt-kluez').getElementsByClassName('pattern')[0];
+
+		var colGroup = '';
+		var row = '';
+
+		for (y in 0...yTotal) {
+			colGroup = '<colgroup>';
+			row += '<tr>';
+			// your code
+			for (x in 0...xTotal) {
+				colGroup += '<col>';
+				row += '<td><span class="table-cell"></span></td>';
+			}
+			colGroup += '<colgroup>';
+			row += '</tr>';
+		}
+		var t = '<table class="table-kluez">\n${colGroup}\n${row}</table>';
+
+		var frag = document.createRange().createContextualFragment(t);
+		table0.appendChild(frag);
 	}
 
 	static public function main() {
